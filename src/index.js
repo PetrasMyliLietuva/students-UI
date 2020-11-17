0ba40dfff3c3ac65574fb9718705e36a1421d31a
agne-zavackyte <a.zavackyte@gmail.com>
2020-11-17 23:34:00 +0200

    global style in styled-components added

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Routes from "./Routes";
-import { ThemeProvider } from "styled-components";
+import { ThemeProvider, createGlobalStyle } from "styled-components";
 import "normalize.css";
 import theme from "./theme";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: ${(props) => props.theme.palette.lightBrown};
+  }
+`;
 
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <Routes />
+      <GlobalStyle />
     </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
