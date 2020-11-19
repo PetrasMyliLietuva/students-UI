bc4ff6f4291b0809a4fa429b8c5b154f62a4cf00
pakstas <pakstas@github.com>
2020-11-19 19:45:22 +0200

    Added fetch to about

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,13 +1,45 @@
-import React from "react";
-import { Section } from "../../components/";
+import React, { useEffect, useState } from "react";
+import { Card, Section } from "../../components/";
+import * as S from "./About.style";
+
+// function Register(studentid, date) {
+//   fetch("http://127.0.0.1:8080/register", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify({ id: Number(studentid), date: date }),
+//   });
+// }
 
 function About() {
+  const [studentsList, setStudentsList] = useState([]);
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8080/attendance")
+      .then((res) => res.json())
+      .then((data) => setStudentsList(data));
+  }, []);
+
   return (
     <Section>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
-      laboriosam incidunt explicabo laudantium pariatur quae, dolore, impedit
-      dignissimos, architecto voluptatibus saepe quisquam repudiandae excepturi
-      fugiat numquam assumenda ut cumque labore.
+      <S.Heading>
+        Attendance on {new Date().toLocaleDateString("lt-LT")}
+      </S.Heading>
+      <S.Wrapper>
+        {studentsList.map((x) => (
+          <S.MarginBlock key={x.id}>
+            <Card
+              name={x.name}
+              surname={x.surname}
+              id={x.id}
+              handleChange={(e) => {
+                console.log(e.target.value);
+              }}
+            />
+          </S.MarginBlock>
+        ))}
+      </S.Wrapper>
     </Section>
   );
 }
---
+++ src/pages/About/About.style.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  margin: 0 auto;
+  text-align: center;
+  max-width: 500px;
+`;
+
+export const Heading = styled.h1`
+  color: ${(props) => props.theme.palette.darkBrown};
+  text-align: center;
+  line-height: 2;
+`;
+
+export const MarginBlock = styled.div`
+  margin-bottom: 5px;
+  &&:last-of-type {
+    margin-bottom: 40px;
+  }
+`;
